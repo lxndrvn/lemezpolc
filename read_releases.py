3cc50163aebe9e19ef7de2d10d667e256bd47fa8
lxndrvn <i1990522@gmail.com>
2017-08-11 23:51:46 +0200

    Remove file writing from collect_releases

--- read_releases.py
+++ read_releases.py
@@ -4,23 +4,21 @@ import re
 pattern = re.compile('(.*)\s-\s(.*)\s*\((\d{4})\)')
 
 def collect_releases(path):
-    with open("data.csv", "w") as output_file:
-        for root, dirs, files in os.walk(path):
-            for name in dirs:
-                details = get_details(name)
-                if not details:
-                    print(name)
-                    continue
-                output_file.write(details)
-                output_file.write("\n")
+    releases = []
+    for root, dirs, files in os.walk(path):
+        for name in dirs:
+            details = get_details(name)
+            if not details:
+                print(name)
+                continue
+            releases.append(details)
+    return releases
+
 
 def get_details(name):
     match = re.match(pattern, name)
     if not match:
         return None
     artist, title, year = match.groups()
-    artist = artist.strip()
-    title = title.strip()
-    line = ",".join((artist, title, year))
-    return line
-
+    details = {'artist': artist.strip(), 'title': title.strip(), 'year': year}
+    return details
