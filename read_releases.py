447c1013277c7b8f47a7a61444656ffaf4100cdc
lxndrvn <i1990522@gmail.com>
2017-08-11 23:17:17 +0200

    Move release collecting into function

--- read_releases.py
+++ read_releases.py
@@ -3,16 +3,18 @@ import re
 
 pattern = re.compile('(.*)\s-\s(.*)\s*\((\d{4})\)')
 
-with open("data.csv", "w") as f:
-    for root, dirs, files in os.walk("/Users/lxndrvn/Downloads/Zene"):
-        for name in dirs:
-            match = re.match(pattern, name)
-            if not match:
-                print(name)
-                continue
-            artist, title, year = match.groups()
-            artist = artist.strip()
-            title = title.strip()
-            line = ",".join((artist, title, year))
-            f.write(line)
-            f.write("\n")
+def collect_releases(path):
+    with open("data.csv", "w") as f:
+        for root, dirs, files in os.walk(path):
+            for name in dirs:
+                match = re.match(pattern, name)
+                if not match:
+                    print(name)
+                    continue
+                artist, title, year = match.groups()
+                artist = artist.strip()
+                title = title.strip()
+                line = ",".join((artist, title, year))
+                f.write(line)
+                f.write("\n")
+
