74fa1cf20959e3b4b071d25857e3310cc80189d4
lxndrvn <i1990522@gmail.com>
2017-08-11 23:38:26 +0200

    Refactor release collecting function

--- read_releases.py
+++ read_releases.py
@@ -4,17 +4,23 @@ import re
 pattern = re.compile('(.*)\s-\s(.*)\s*\((\d{4})\)')
 
 def collect_releases(path):
-    with open("data.csv", "w") as f:
+    with open("data.csv", "w") as output_file:
         for root, dirs, files in os.walk(path):
             for name in dirs:
-                match = re.match(pattern, name)
-                if not match:
+                details = get_details(name)
+                if not details:
                     print(name)
                     continue
-                artist, title, year = match.groups()
-                artist = artist.strip()
-                title = title.strip()
-                line = ",".join((artist, title, year))
-                f.write(line)
-                f.write("\n")
+                output_file.write(details)
+                output_file.write("\n")
+
+def get_details(name):
+    match = re.match(pattern, name)
+    if not match:
+        return None
+    artist, title, year = match.groups()
+    artist = artist.strip()
+    title = title.strip()
+    line = ",".join((artist, title, year))
+    return line
 
