24589d0e2d248c059a4a0ad6ea08eac8978589d7
lxndrvn <i1990522@gmail.com>
2017-08-15 23:37:55 +0200

    Refactor and improve Discogs scraper

--- scrape_discogs_data.py
+++ scrape_discogs_data.py
@@ -13,11 +13,15 @@ SEARCH_URL = 'https://api.discogs.com/database/search'
 
 
 def get_release_data(release):
-    response = send_request(SEARCH_URL, params={'artist': release['artist'],
-                                                'release_title': release['title']})
-    image_link = response.json()['results'][0]['thumb']
-    release['discogs_link'] = response.json()['results'][0]['resource_url']
-    release['image'] = download_image(image_link, release['directory'])
+    release_by_search = get_release_by_search(release)
+    release['format'] = get_release_format(release_by_search['format'])
+    
+    release_by_url = get_release_by_api_url(release_by_search['resource_url'])
+    release['discogs_link'] = release_by_url['uri']
+
+    if not any(file.endswith(".jpg") for file in release['directory']):
+        release['image'] = get_image(release_by_url, release['directory'])
+
     return release
 
 
@@ -26,6 +30,46 @@ def send_request(url, params=None):
     return response
 
 
+def get_release_by_search(release):
+    response = send_request(
+        SEARCH_URL,
+        params={'artist': release['artist'], 'release_title': release['title']}
+    )
+    return get_matching_release(response, release['year'])
+    
+    
+def get_matching_release(response, year):
+    all_releases = response.json()['results']
+    for version in all_releases:
+        if version['year'] == year:
+            return version
+
+def get_release_format(discogs_release_format):
+    if 'Album' in discogs_release_format:
+        return 'ALBUM'
+    if 'Mini-Album' in discogs_release_format:
+        return 'MINI-ALBUM'
+    if 'EP' in discogs_release_format:
+        return 'EP'
+    
+    print('Could not find format in {0}'.format(discogs_release_format))
+    return None
+
+def get_release_by_api_url(url):
+    response = send_request(url)
+    return response.json()
+
+
+def get_image(release, directory):
+    image = get_primary_image(release['images'])
+    return download_image(image['uri'] , directory)
+    
+def get_primary_image(images):
+    for image in images:
+        if image['type'] == 'primary':
+            return image
+    return images[0]
+    
 def download_image(url, directory):
     filename = directory + '/folder.jpg'
     response = send_request(url)
