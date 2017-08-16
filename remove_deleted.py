e568b26b4d8b6ac09c4d6aa06b032578b3d2d221
lxndrvn <i1990522@gmail.com>
2017-08-16 15:53:07 +0200

    Log deleted release

--- remove_deleted.py
+++ remove_deleted.py
@@ -4,7 +4,9 @@ from models import Release
 def remove_deleted_data():
     for release in Release.select():
         if not os.path.exists(release.directory):
+           print('Deleted release {0}'.format(release.directory))
            release.delete_instance()
+    return
 
 
 remove_deleted_data()
