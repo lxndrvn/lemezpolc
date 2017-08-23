1c9d81198f3c9c7757501a3911b956f2b76c64d1
lxndrvn <i1990522@gmail.com>
2017-08-23 17:16:59 +0200

    Add Release model

--- lemezpolc/models.py
+++ lemezpolc/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Release(models.Model):
+    artist = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    year = models.IntegerField()
+    cover = models.BinaryField(blank=True, null=True)
+    discogs_link = models.CharField(max_length=255, blank=True, null=True)
+    directory = models.CharField(max_length=255, blank=True, null=True)
+    format = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'release'
