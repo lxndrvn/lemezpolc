679fee0f6e319cb4860ceadc0f42626e5ac8f40d
lxndrvn <i1990522@gmail.com>
2017-08-23 22:12:27 +0200

    Change cover to cover path

--- lemezpolc/models.py
+++ lemezpolc/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Release(models.Model):
     artist = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     year = models.IntegerField()
-    cover = models.BinaryField(blank=True, null=True)
+    cover_path = models.CharField(max_length=255, blank=True, null=True)
     discogs_link = models.CharField(max_length=255, blank=True, null=True)
     directory = models.CharField(max_length=255, blank=True, null=True)
     format = models.CharField(max_length=255, blank=True, null=True)
