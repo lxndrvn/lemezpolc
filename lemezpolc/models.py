1eaec843edfc650b45aff2cf8773c795cfa7f82e
lxndrvn <i1990522@gmail.com>
2017-08-24 23:14:36 +0200

    Rename cover path to cover

--- lemezpolc/models.py
+++ lemezpolc/models.py
@@ -4,7 +4,7 @@ class Release(models.Model):
     artist = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     year = models.IntegerField()
-    cover_path = models.CharField(max_length=255, blank=True, null=True)
+    cover = models.CharField(max_length=255, blank=True, null=True)
     discogs_link = models.CharField(max_length=255, blank=True, null=True)
     directory = models.CharField(max_length=255, blank=True, null=True)
     format = models.CharField(max_length=255, blank=True, null=True)
