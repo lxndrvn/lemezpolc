8733049e10d47d81fa2ab5fd52d458f36d451cb8
lxndrvn <i1990522@gmail.com>
2017-08-26 21:00:28 +0200

    Add simple Bootstrap design

--- lemezpolc/views.py
+++ lemezpolc/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from lemezpolc.models import Release
+
+
+def index(request):
+    releases = Release.objects.order_by('artist')[:30]
+    return render(request, 'lemezpolc/index.html', {'releases': releases})
