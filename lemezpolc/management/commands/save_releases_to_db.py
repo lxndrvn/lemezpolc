5fe77c2f2e9f0d4ea26c0b4f1912330344d51389
lxndrvn <i1990522@gmail.com>
2017-08-27 21:13:08 +0200

    Remove try-except from saving releases

--- lemezpolc/management/commands/save_releases_to_db.py
+++ lemezpolc/management/commands/save_releases_to_db.py
@@ -17,19 +17,16 @@ class Command(BaseCommand):
                 time.sleep(3)
 
     def create_release(self, release):
-        try:
-            updated_release = get_release_data(release)
-            Release.objects.create(artist=updated_release.get('artist'),
-                                   title=updated_release.get('title'),
-                                   year=updated_release.get('year'),
-                                   discogs_link=updated_release.get('discogs_link'),
-                                   cover=updated_release.get('cover'),
-                                   directory=updated_release.get('directory'),
-                                   format=updated_release.get('format'))
-            print('Created release {0} - {1} - {2}'.format(
-                updated_release['artist'], updated_release['title'], updated_release['year']))
-        except:
-            pass
+        updated_release = get_release_data(release)
+        Release.objects.create(artist=updated_release.get('artist'),
+                               title=updated_release.get('title'),
+                               year=updated_release.get('year'),
+                               discogs_link=updated_release.get('discogs_link'),
+                               cover=updated_release.get('cover'),
+                               directory=updated_release.get('directory'),
+                               format=updated_release.get('format'))
+        print('Created release {0} - {1} - {2}'.format(
+            updated_release['artist'], updated_release['title'], updated_release['year']))
 
     def is_in_database(self, release):
         db_record = Release.objects.filter(artist=release['artist'], title=release['title'])
