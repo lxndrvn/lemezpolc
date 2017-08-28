e75dbbd2bc2340d3f9bc9694e68eba3fde6aaf54
lxndrvn <i1990522@gmail.com>
2017-08-28 22:44:31 +0200

    Match year for releases found by title search

--- lemezpolc/jobs/scrape_discogs_data.py
+++ lemezpolc/jobs/scrape_discogs_data.py
@@ -58,11 +58,13 @@ def send_request(url, params=None):
 def get_release_by_search(release):
     results = get_results(SEARCH_URL, params={'artist': release.artist, 'release_title': release.title})
     if not results:
-        return get_release_by_title_match(release)
-    return get_matching_release(results, release.year)
+        results = get_release_by_title_search(release)
+        if not results:
+            return None
+    return get_match_by_year(results, release.year)
 
 
-def get_matching_release(results, year):
+def get_match_by_year(results, year):
     for version in results:
         if version.get('year') == year:
             return version
@@ -78,32 +80,28 @@ def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-def get_release_by_title_match(release):
+def get_release_by_title_search(release):
     # Title at Discogs means artist and release title, like "G.S. Schray - Gabriel"
 
-    results = get_results(SEARCH_URL, params={'release_title': release.title, 'year': release.year})
+    results = get_results(SEARCH_URL, params={'release_title': release.title})
     if not results:
         print('No Discogs match found for {0} - {1} - {2}'.format(release.artist, release.title, release.year))
         return None
 
-    full_title = '{0} - {1}'.format(release.artist, release.title)
-    matches = [(similarity(full_title, result['title']), result) for result in results]
-    return get_best_match(full_title, matches)
+    if release.artist == 'VA':
+        full_title = 'Various - {0}'.format(release.title)
 
-
-def get_best_match(full_title, matches):
-    match_ratio, matching_result = max(matches, key=lambda match: match[0])
-    if match_ratio > 0.9:
-        return matching_result
     else:
-        if is_various_artists(full_title, matching_result['title']) and match_ratio > 0.7:
-            return matching_result
-    raise DiscogsException('Could not find good match for {0}. Best match: {1}, {2}'.format(
-                            full_title, matching_result['title'], match_ratio))
+        full_title = '{0} - {1}'.format(release.artist, release.title)
+
+    matches = [(similarity(full_title, result['title']), result) for result in results]
+    return get_best_match(full_title, matches, release)
 
 
-def is_various_artists(title, match_title):
-    return title.startswith('VA') and match_title.startswith('Various')
+def get_best_match(full_title, matches, release):
+    possible_matches = [match[1] for match in matches if match[0] > 0.9]
+    print('Matches for {0}: {1}'.format(full_title, possible_matches))
+    return possible_matches
 
 
 def get_release_format(discogs_release_formats):
