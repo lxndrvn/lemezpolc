ed5eab00b9789882325e5fb474726754d19f7c69
lxndrvn <i1990522@gmail.com>
2017-08-28 21:25:10 +0200

    Case insensitive sorting

--- lemezpolc/views.py
+++ lemezpolc/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.db.models.functions import Lower
 
 from lemezpolc.models import Release
 
 
 def index(request):
-    releases = Release.objects.order_by('artist')
+    releases = Release.objects.order_by(Lower('artist'))
+    return render(request, 'lemezpolc/index.html', {'releases': releases})
+
     return render(request, 'lemezpolc/index.html', {'releases': releases})
