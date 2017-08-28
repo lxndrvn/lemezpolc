230b6feab2898fc920a0261efebab6825537bdc3
lxndrvn <i1990522@gmail.com>
2017-08-28 21:24:21 +0200

    Move is_in_database to commons

--- lemezpolc/management/commands/save_releases_to_db.py
+++ lemezpolc/management/commands/save_releases_to_db.py
@@ -6,6 +6,7 @@ from lemezpolc.config import DEFAULT_PATH
 from lemezpolc.jobs.read_releases import collect_releases
 from lemezpolc.jobs.resize_image import resize_image
 from lemezpolc.jobs.scrape_discogs_data import extend_release
+from lemezpolc.management.common import is_in_database
 from lemezpolc.models import Release
 
 
@@ -13,7 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         releases = collect_releases(DEFAULT_PATH)
         for release in releases:
-            if not self.is_in_database(release):
+            if not is_in_database(release):
                 self.create_release(release)
                 time.sleep(3)
 
@@ -35,6 +36,3 @@ class Command(BaseCommand):
         print('Created release {0} - {1} - {2}'.format(
             release.artist, release.title, release.year))
 
-    def is_in_database(self, release):
-        db_record = Release.objects.filter(artist=release.artist, title=release.title)
-        return db_record.exists()
