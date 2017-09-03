52a8b79675e46b2f6cfb9303f04b5478d550c6de
lxndrvn <i1990522@gmail.com>
2017-09-03 22:00:46 +0200

    Add release counter

--- lemezpolc/views.py
+++ lemezpolc/views.py
@@ -7,4 +7,6 @@ from lemezpolc.models import Release
 
 def release_list(request):
     release_filter = ReleaseFilter(request.GET, queryset=Release.objects.order_by(Lower('artist')))
-    return render(request, 'lemezpolc/index.html', {'filter': release_filter})
+    release_counter = Release.objects.count()
+    return render(request, 'lemezpolc/index.html',
+                  {'filter': release_filter, 'counter': release_counter})
